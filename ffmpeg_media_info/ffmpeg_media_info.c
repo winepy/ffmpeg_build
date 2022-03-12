a361f60083d212d149daac7301a068261683f030
winepy <16655180846@163.com>
2022-03-12 18:17:00 +0800

    视频信息

---
+++ ffmpeg_media_info/ffmpeg_media_info.c
@@ -0,0 +1,28 @@
+//
+
+#include <libavutil/avutil.h>
+#include <libavformat/avformat.h>
+
+int main(){
+    AVFormatContext *fmt_ctx = NULL;
+    av_log_set_level(AV_LOG_DEBUG);
+
+    int ret ;
+    avformat_network_init();//av_register_all()过期了
+
+    ret = avformat_open_input(&fmt_ctx, "./test.mp4",NULL,NULL);
+
+    if(ret<0){
+        av_log(NULL,AV_LOG_ERROR,"cannt open file %s \n",av_err2str(ret));
+        return -1;
+    }
+
+    av_dump_format(fmt_ctx,0,"./test.mp4",0);//打印meta信息
+
+    avformat_close_input(&fmt_ctx);
+
+
+
+
+    return 0;
+}
